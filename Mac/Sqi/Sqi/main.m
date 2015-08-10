1d81feb59eb3c6602b754b11f520eb32c44d5e33
Egor Chiglintsev <wanderw@yandex-team.ru>
2015-08-10 21:52:23 +0600

    Added empty Mac console app project.

---
+++ Mac/Sqi/Sqi/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Sqi
+//
+//  Created by Egor Chiglintsev on 10.08.15.
+//  Copyright (c) 2015 Egor Chiglintsev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
