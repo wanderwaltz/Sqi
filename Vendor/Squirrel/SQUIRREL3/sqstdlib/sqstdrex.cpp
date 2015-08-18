e02f9b5c56cab092899a9aab5a7690134b4deb3d
Egor Chiglintsev <wanderw@yandex-team.ru>
2015-08-18 16:59:15 +0600

    Fix 'value stored to first is never read' warning.

--- Vendor/Squirrel/SQUIRREL3/sqstdlib/sqstdrex.cpp
+++ Vendor/Squirrel/SQUIRREL3/sqstdlib/sqstdrex.cpp
@@ -205,7 +205,6 @@ static SQInteger sqstd_rex_class(SQRex *exp)
 	if(first!=-1){
 		SQInteger c = first;
 		exp->_nodes[chain].next = c;
-		first = -1;
 	}
 	/* hack? */
 	exp->_nodes[ret].left = exp->_nodes[ret].next;
