1de2d59f2074d7efd84cd7be70b89ccfb37c5cd6
Egor Chiglintsev <wanderw@yandex-team.ru>
2015-08-23 23:54:54 +0600

    Squashed 'Vendor/SqXtdLib/' content from commit 14988f7
    git-subtree-dir: Vendor/SqXtdLib
    git-subtree-split: 14988f7a0cf71ff5c02dca31fedb12a055be4b62

---
+++ SqXtdLib/SqXtdLib/sqxtd_utils.cpp
@@ -0,0 +1,41 @@
+//
+//  sqxtd_utils.cpp
+//  SqXtdLib
+//
+//  Created by Egor Chiglintsev on 23.08.15.
+//  Copyright (c) 2015 Egor Chiglintsev. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+#include "sqxtd_utils.h"
+#include "SqXtdLib.h"
+#include "string.h"
+
+static SQRESULT sqxtd_native_getdefaultdelegate(HSQUIRRELVM vm) {
+    sq_getdefaultdelegate(vm, sq_gettype(vm, -1));
+    return 1;
+}
+
+void sqxtd_register_getdefaultdelegate(HSQUIRRELVM vm) {
+    const SQChar *name = "getdefaultdelegate";
+    
+    sq_pushstring(vm, name, strlen(name));
+    sq_newclosure(vm, &sqxtd_native_getdefaultdelegate, 0);
+    sq_newslot(vm, -3, SQFalse);
+}
